f9cb3b1139ea207f8c5ae8996aae6347b2da9c15
sorchard <sorchard001@gmail.com>
2017-05-14 12:05:21 +0100

    Simplified palette change algorithm

--- colour_change.asm
+++ colour_change.asm
@@ -12,7 +12,7 @@
 ; Each alternate palette is packed into a byte DDCCBBAA
 ; AA is the new colour[0], BB is colour[1] etc.
 ; i.e. after the palette change the palette bytes will be AA,BB,CC,DD
-; 
+;
 ; A 4 byte mapping table is created.
 ; The current palette decides where in the map the new colours go e.g.:
 ;  Current palette 00,01,10,11 -> map AA,BB,CC,DD
@@ -35,7 +35,7 @@ next_colour_set	rmb 2	; pointer to palette
 
 
 	section "DATA"
-	
+
 current_pal_tiles 	rmb 4
 
 
@@ -53,54 +53,48 @@ colour_change_init
 	rts
 
 ; create 256 byte lookup table
-; U points to current palette
+; X points to current palette
 ; A contains new palette
 ; returns with Y pointing to middle of lookup table
 setup_colour_change_table
 
-	ldx td_fbuf		; use screen buffer for storage while nobody is looking
+	ldu td_fbuf		; use screen buffer for storage while nobody is looking
 	sta temp1		; palette for unpacking
 
 	; determine new colour mapping
-	; 
+	;
 	lda #4
 	sta temp0
-1	lda ,u		; current palette entry
+1	lda ,x		; current palette entry
 	ldb temp1	; get new colour for palette entry
 	andb #3		;
-	stb ,u+		; update palette entry
-	stb a,x		; store in new mapping
+	stb ,x+		; update palette entry
+	stb a,u		; store in new mapping
 	lsr temp1	; shift next packed colour
 	lsr temp1	;
 	dec temp0
 	bne 1b
-	
+
 	; generate 256 byte lookup table
-	; uses recursion to produce every permutation of colours
 
-	leau 4,x		; point to mapping table+4
-	leay 4+128,x	; point to middle of lookup table
-	ldb #5			; set recursion depth
-	stb temp0		;
+	leay 4+128,u	; point to middle of lookup table
+	clr temp0
 
-_cc_add_bits
-	pshs a,b
-	dec temp0	; deeper level of recursion
-	beq 2f		; deep enough so store byte
-	ldb #-4		; reset counter
-	lsla		; move existing bits out of way
+5	lda temp0
+	ldx #4		; 4 pixels per byte
+1	clrb		; get one pixel from A into B
 	lsla		;
-1	ora b,u		; combine entry from mapping table
-	bsr _cc_add_bits
-	anda #$fc	; clear bits for next loop
-	incb		; point to next entry in mapping table
-	bne 1b		;
-	inc temp0	; reduce recursion level
-	puls a,b,pc	; 
+	rolb		;
+	lsla		;
+	rolb		;
+	ora b,u		; put recoloured pixel back into A
+	leax -1,x	;
+	bne 1b		; next pixel
 2	sta <0,y	; use offset instead of auto-inc to get required layout
 	inc 2b+2	; (note 256 byte table means offset returns to 0)
 	inc temp0
-	puls a,b,pc
+	bne 5b		; next byte
+	rts
 
 
 ; apply next palette to tile data and shift buffers
@@ -113,7 +107,7 @@ colour_change_x
 	leax (colour_set_table-colour_set_table_end),x
 1	stx next_colour_set
 
-	ldu #current_pal_tiles
+	ldx #current_pal_tiles
 	jsr setup_colour_change_table
 	; y is now pointing to middle of lookup table
 
@@ -123,7 +117,7 @@ colour_change_x
 	ldx #TD_SBUFF
 	ldd #TD_SBEND
 	; fall through to _colour_change_sub
-	
+
 _colour_change_sub
 	std 2f+1
 1	ldd ,x
@@ -134,7 +128,7 @@ _colour_change_sub
 	blo 1b
 	rts
 
-	
+
 GREEN	equ 0
 YELLOW	equ 1
 BLUE	equ 2
